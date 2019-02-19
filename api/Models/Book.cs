d2515ec2bc4f261a1e9c3dcec389a1b2b06d390f
Joey David <david.156@osu.edu>
2019-02-19 17:00:25 -0500

    books controller created

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -4,7 +4,9 @@ namespace Fisher.Bookstore.Models
 {
     public class Book
     {
-        public int BookId {get;set;}
-        public string Name {get;set;}
+        public int Id {get;set;}
+        public string Title {get;set;}
+        public string Author {get;set;}
+        public string ISBN {get;set;}
     }
 }
