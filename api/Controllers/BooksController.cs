b3614b6dce30a7a58e2428807f2bf9b6cec29b50
Joey David <david.156@osu.edu>
2019-02-20 14:19:18 -0500

    implemented code to get all books

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -41,5 +41,10 @@ namespace Fisher.Bookstore.Api.Controllers
                 this.db.SaveChanges();
             }
         }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(db.Books);
+        }
     }
 }
